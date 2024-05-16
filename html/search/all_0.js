9219c0c0cc1d494df6789ce618995727cf2798bc
Hugo Verde <hugo.verde@ua.pt>
2024-05-16 23:47:51 +0100

    Entire project

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['buttonpressed_0',['buttonPressed',['../main_8c.html#a8c7506a587944d32c608388aaec2eff1',1,'main.c']]]
+];
---
+++ html/search/functions_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['vm_5fdispense_5fproduct_10',['vM_dispense_product',['../main_8c.html#ab4af57d2cce3c7dba91ed52e2f29f1b0',1,'main.c']]],
+  ['vm_5freturn_5fcredit_11',['vM_return_credit',['../main_8c.html#a5ea25af90af80f39cf98be5871f11217',1,'main.c']]],
+  ['vm_5fselect_5fproduct_12',['vM_select_product',['../main_8c.html#aa758ee88066a1e37cd6f0f7552503b87',1,'main.c']]],
+  ['vm_5fwelcome_5fmessage_13',['vM_welcome_message',['../main_8c.html#afd11a86b965b555a30f121db88a8e0a1',1,'main.c']]]
+];
